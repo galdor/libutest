48e104f3390ac2047c5f322aa032cbaf4b44dd66
Nicolas Martyanoff <khaelin@gmail.com>
2014-08-20 01:27:48 +0200

    escape strings before printing them in the terminal

--- src/terminal.c
+++ src/terminal.c
@@ -14,19 +14,29 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <ctype.h>
 #include <stdio.h>
 
 #include "utest.h"
 
+static char *test_escape_string_for_display(const char *);
+
 void
 test_report_terminal(FILE *output, const char *test_name, bool success,
                      const char *file, int line, const char *errmsg) {
+
     if (success) {
         fprintf(output, "\e[32m.\e[0m %-24s  \e[32mok\e[0m\n",
                 test_name);
     } else {
+        char *escaped_errmsg;
+
+        escaped_errmsg = test_escape_string_for_display(errmsg);
+
         fprintf(output, "\e[31mx\e[0m %-24s  %s:%d  \e[31m%s\e[0m\n",
-                test_name, file, line, errmsg);
+                test_name, file, line, escaped_errmsg);
+
+        free(escaped_errmsg);
     }
 }
 
@@ -57,3 +67,39 @@ test_print_results_terminal(FILE *output, size_t nb_tests,
             nb_failed_tests, ratio_failed * 100.0);
 }
 
+static char *
+test_escape_string_for_display(const char *string) {
+    char *escaped_string, *optr;
+    const char *iptr;
+
+    /* Even when escaping each character with its value (\xxx), we will never
+     * use more than strlen(errmsg) * 4 bytes, plus 1 for the final \0 */
+    escaped_string = malloc(strlen(string) * 4 + 1);
+    optr = escaped_string;
+
+    for (iptr = string; *iptr != '\0'; iptr++) {
+        switch (*iptr) {
+        case '\a': *optr++ = '\\'; *optr++ = 'a'; break;
+        case '\b': *optr++ = '\\'; *optr++ = 'b'; break;
+        case '\t': *optr++ = '\\'; *optr++ = 't'; break;
+        case '\n': *optr++ = '\\'; *optr++ = 'n'; break;
+        case '\v': *optr++ = '\\'; *optr++ = 'v'; break;
+        case '\f': *optr++ = '\\'; *optr++ = 'f'; break;
+        case '\r': *optr++ = '\\'; *optr++ = 'r'; break;
+
+        default:
+            if (isprint((unsigned char)*iptr)) {
+                *optr++ = *iptr;
+            } else {
+                *optr++ = '\\';
+                sprintf(optr, "%03u", (unsigned char)(*iptr));
+                optr += 3;
+            }
+            break;
+        }
+    }
+
+    *optr++ = '\0';
+
+    return escaped_string;
+}
